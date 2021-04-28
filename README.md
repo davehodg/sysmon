fac7cc35c2aee96188b1ba581a72263040fb09d1
David Hodgkinson <daveh@hodgkinson.org>
2021-04-28 15:46:47 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # sysmon
-Simple flask app to see the stats of a remote machine
+Simple flask app to see the stats of a remote machine.
+
+It's a de-classed version of
+https://hackersandslackers.com/automate-ssh-scp-python-paramiko/
+
+I'd strongly recommend using paython3.
+
+If I have to use it in anger, I'll re-class it. Also, keep variables
+in a configfile. Or secrets in github if I set an action.
+
+Set:
+
+FLASK_ENV=development
+FLASK_APP=sysmon.py
+
+Then do:
+
+flask run
+
---
+++ sysmon.py
@@ -0,0 +1,55 @@
+from flask import Flask
+app = Flask(__name__)
+
+from os import system
+from paramiko import SSHClient, AutoAddPolicy, RSAKey
+from paramiko.auth_handler import AuthenticationException, SSHException
+from scp import SCPClient, SCPException
+
+
+@app.route('/sysmon')
+def sysmon():
+    return remote_call("hostname")
+
+def remote_call(command):
+    host = "polarbear.vs.mythic-beasts.com"
+    username = "root"
+    key_path = "/Users/daveh/.ssh/id_rsa"
+
+    try:
+        ssh_key = RSAKey.from_private_key_file(key_path)
+        print("Found SSH key at self {key_path}")
+    except SSHException as e:
+        print(e)
+
+    try:
+        system("ssh-copy-id -i {key_path}.pub {username}@{host}>/dev/null 2>&1")
+        print("{key_path} uploaded to {host}")
+    except FileNotFoundError as error:
+        print(error)
+
+    try:
+        client = SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(AutoAddPolicy())
+        client.connect(
+            host,
+            username=username,
+            password="",
+            key_filename=key_path,
+            timeout=5000,
+            )
+    except AuthenticationException as e:
+        print("Authentication failed: did you remember to create an SSH key? {e}")
+        raise e
+
+    stdin, stdout, stderr = client.exec_command(command)
+    stdout.channel.recv_exit_status()
+    response = stdout.readlines()
+    for line in response:
+        print(line)
+
+    if client:
+        client.close()
+
+    return line
